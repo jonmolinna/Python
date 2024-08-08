e3e9c86e865bfadba21d54a3b9e1e6f0ec74fe3a
jonmolinna <jmolina2624@gmail.com>
2024-08-08 15:50:04 -0500

    Funciones

--- 1-basico/95-funciones.py
+++ 1-basico/95-funciones.py
@@ -10,3 +10,24 @@ def sumar(a, b):
 result = sumar(10, 20)
 print(result)
 
+# *args => tuplas
+def sumar2(*args):
+    suma = 0
+
+    for i in args:
+        suma += i
+
+    return suma
+
+result = sumar2(1, 2, 3, 4)
+print(result)
+
+# **kwargs => diccionario
+def names(**kwargs):
+    # print(kwargs['name'] + ' ' + kwargs['surname'])
+    for key, value in kwargs.items():
+        print(value, end=' ')
+
+names(name='Kendra', surname='Contreras', age=28)
+
+
